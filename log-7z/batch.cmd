e71fadb5ed3b0b9ddb942027ac0a8db0d7a24adf
echochio <echochio-tw@users.noreply.github.com>
2020-02-18 15:32:55 +0800

    Update batch.cmd

--- log-7z/batch.cmd
+++ log-7z/batch.cmd
@@ -1,2 +1,5 @@
-FOR /F "usebackq delims=?" %i IN (`DIR C:\7-11\Log /B`) DO c:\log-7z\7z.exe a -tzip "c:\log-7z\%~ni.zip" "C:\7-11\Log\%i"
+c:
+cd c:\log-7z
+for /d %%i in (c:\7-11\log\*) do c:\log-7z\7z.exe a -tzip c:\log-7z\log.zip %%i
+timeout /t 30
 forfiles /s /p "C:\7-11\Log" /d -7 -c "cmd /c del /f /q @file"
